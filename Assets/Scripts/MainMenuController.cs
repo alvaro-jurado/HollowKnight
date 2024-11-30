388e7fea8665a1d2e8bdea7f6eb516c5f54c8a1c
Alvaro Jurado Cano <alvarojurcan@campus.monlau.com>
2024-11-30 13:19:36 +0100

    Resolution settings implemented

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -7,6 +7,8 @@ public class MainMenuController : MonoBehaviour
 {
     public Canvas mainCanvas;
     public Canvas optionsCanvas;
+    public Canvas audioCanvas;
+    public Canvas videoCanvas;
     public Animator fleurAnimator;
     public void NewGame()
     {
@@ -20,10 +22,38 @@ public class MainMenuController : MonoBehaviour
         fleurAnimator.SetTrigger("OpenFleur");
     }
 
+    public void Audio()
+    {
+        mainCanvas.GetComponent<Canvas>().enabled = false;
+        optionsCanvas.GetComponent<Canvas>().enabled = false;
+        videoCanvas.GetComponent<Canvas>().enabled = false;
+        audioCanvas.GetComponent<Canvas>().enabled = true;
+        fleurAnimator.SetTrigger("OpenFleur");
+    }
+
+    public void Video()
+    {
+        mainCanvas.GetComponent<Canvas>().enabled = false;
+        optionsCanvas.GetComponent<Canvas>().enabled = false;
+        videoCanvas.GetComponent<Canvas>().enabled = true;
+        audioCanvas.GetComponent<Canvas>().enabled = false;
+        fleurAnimator.SetTrigger("OpenFleur");
+    }
+
     public void Back()
     {
         optionsCanvas.GetComponent<Canvas>().enabled = false;
         mainCanvas.GetComponent<Canvas>().enabled = true;
+        videoCanvas.GetComponent<Canvas>().enabled = false;
+        audioCanvas.GetComponent<Canvas>().enabled = false;
+        fleurAnimator.SetTrigger("CloseFleur");
+    }
+    public void BackOptions()
+    {
+        optionsCanvas.GetComponent<Canvas>().enabled = true;
+        mainCanvas.GetComponent<Canvas>().enabled = false;
+        audioCanvas.GetComponent<Canvas>().enabled = false;
+        videoCanvas.GetComponent<Canvas>().enabled = false;
         fleurAnimator.SetTrigger("CloseFleur");
     }
 
